45f5e633139bc50df920b405d8047b821eecb435
Yaksh Bariya <yakshbari4@gmail.com>
2025-12-18 00:41:59 +0530

    ci: run on ubuntu-slim
    Effectively a revert of 13a58ae415895e62613edc031bc3f21acab1681d (for
    removing the concurrency logic)
    
    Also get rid of concurrency in generate-repology-metadata.sh (man it
    looks terrible when I look back at what horrible code I wrote)
    
    ubuntu-slim has just 1 vCPU instead of 4 vCPU for ubuntu-latest. Runners
    are limited to 15 minutes. We average around 1 minute with 4 cores for
    generation of packages.json, with just 1 core, it should be around 4
    minutes which is well under 15 minutes.
    
    ubuntu-slim was available since 28 October 2025: https://github.blog/changelog/2025-10-28-1-vcpu-linux-runner-now-available-in-github-actions-in-public-preview/
    
    Should reduce our CI usage effectively by atleast some amount (less
    compute wasted)

--- generate-repology-metadata.sh
+++ generate-repology-metadata.sh
@@ -170,32 +170,17 @@ exclude_pkgs=$'\n'"$(env -C $TERMUX_PACKAGES_DIR grep -rlE '^TERMUX_PKG_GENERATE
 	sed -E 's@.*/([^/]+)/build\.sh@\1@')"$'\n'
 
 for repo_path in ${repo_paths[@]}; do
-	i=0
 	package_paths=($TERMUX_PACKAGES_DIR/$repo_path/*)
-	package_paths_count="${#package_paths[@]}"
-	while [ $((i * 100)) -le "${package_paths_count}" ]; do
-		for package_path in "${package_paths[@]:$((i*100)):100}"; do
-			[[ $'\n'"$exclude_pkgs"$'\n' == *$'\n'"${package_path#*/}"$'\n'* ]] && continue
-			if [ "$FIRST" = "yes" ]; then
-				FIRST=no
-			else
-				echo ","
-			fi
-			( check_package $package_path )
-		done > $( printf "/tmp/repology-metadata-%s.%03d" $repo_path $i ) &
-		i=$((i + 1))
-		FIRST=no
+	for package_path in "${package_paths[@]}"; do
+		[[ $'\n'"$exclude_pkgs"$'\n' == *$'\n'"${package_path#*/}"$'\n'* ]] && continue
+		if [ "$FIRST" = "yes" ]; then
+			FIRST=no
+		else
+			echo ","
+		fi
+		( check_package $package_path )
 	done
-	if [ $(jobs -p | grep -c $'\n') -gt $(nproc) ]; then
-		wait -n
-	fi
-done
-wait
-for repo_path in ${repo_paths[@]}; do
-	for file in /tmp/repology-metadata-$repo_path.*; do
-		cat $file
-		rm $file
-	done
+	FIRST=no
 done
 echo ""
 echo "]"
